9489091192135187d148f2f057c4f6e25c9781a3
Orangian <tysayahi@gmail.com>
2023-05-02 08:57:46 -0400

    awar

--- index.php
+++ index.php
@@ -1,17 +1,66 @@
 <?php
 $directory = '/Users/24sayahit/Documents/wd-1';
-
-function scanarama($dir) {
-    $rawdir = array_diff(scandir($dir), array('..', '.'));
-    foreach($rawdir as &$tory) {
-        $isdir = is_dir($tory);
-        $tory = array($tory, if( is_dir($tory) != 1){return 0});
-    }
-    return $rawdir;
+function recurse($value) {
+    
 }
+function scanarama($dir) {
+    //$topdir = array_values(array_diff(scandir($dir), array('..', '.')));
+    $topdir = scandir($dir);
+    foreach($topdir as &$filename) {
+        $isdir = is_dir($filename);
+        $dir = null ;
+        if( is_dir($filename) != 1){
+            $dir = "0";
+            $filename = array($filename, is_dir($filename));
+        } else {
+            $dir = 1;
+            $filename = array($filename, is_dir($filename), scanarama($filename));
+        }
+        
+    }
+    return $topdir;
+}
+
+function scanDir($target) {
+
+    if(is_dir($target)){
+
+        $files = glob( $target . '*', GLOB_MARK ); //GLOB_MARK adds a slash to directories returned
+
+        foreach( $files as $file )
+        {
+            scanDir( $file );
+        }
+
+
+    } 
+}
+
+function pretty_dump($arr, $d=1){
+    if ($d==1) echo "<pre>";    // HTML Only
+    if (is_array($arr)){
+        foreach($arr as $k=>$v){
+            for ($i=0;$i<$d;$i++){
+                echo "\t";
+            }
+            if (is_array($v)){
+                echo $k.PHP_EOL;
+                Pretty_Dump($v, $d+1);
+            } else {
+                echo $k."\t".$v.PHP_EOL;
+            }
+        }
+    }
+    if ($d==1) echo "</pre>";   // HTML Only
+}
+
+$dirscanned = scanDir($directory);
+
+$transversed = pretty_dump($dirscanned);
+
 ?>
 
-<?= print_r(scanarama($directory)); ?>
+<?= $transversed ?>
 
 
 
